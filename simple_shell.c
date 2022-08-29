1f9daed7ea208f8b0df22b7a788840ff490ae87a
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-08-29 15:09:48 -0700

    simple shell

---
+++ simple_shell.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main- main entry point
+ * @argc: number of command line arguments
+ * @argv: NULL terminated array of strings containing the CLI arguments
+ * Return: Always 0 on success
+ */
