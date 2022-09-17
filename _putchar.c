a677171deda76dcfab5212fd366a956e748ed819
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-09-17 13:06:52 -0700

    _putchar

---
+++ _putchar.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+
+#include <unistd.h>
+/**
+ * _putchar - prints a character
+ * @c: is a parameter
+ * Return: 1 if success and -1 if not
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+
+}
