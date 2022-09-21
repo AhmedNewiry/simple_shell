f37309574ea66e793e3f0938359be846199295b7
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-09-21 13:45:26 -0700

    fix bug

--- simple_shell.c
+++ simple_shell.c
@@ -15,19 +15,27 @@ int main(int argc __attribute__((unused)), char **argv, char *env[])
 
 	while (1)
 	{
+		
 		print_prompt(STDIN_FILENO);
 		if (getline(&buffer, &line_len, stdin) == -1)
 		{
+			free(buffer);
 			return (-1);
 		}
+
+		if (buffer[0] == '\n')
+		{                                                           
+
+			continue;
+		}
+
 		for (x = 0; buffer[x] != '\0'; x++)
 		{
-			if (x == 0 && buffer[x] == '\n')
-				free(buffer);
-				continue;
 			n = _strlen(buffer) - 1;
 			if (x == n)
+			{
 				buffer[x] = '\0';
+			}
 		}
 		tokens = mod_strtok(buffer, " ");
 		if (tokens == NULL && argv == NULL)
@@ -36,16 +44,25 @@ int main(int argc __attribute__((unused)), char **argv, char *env[])
 			continue;
 		}
 		else
+		{
+	
 			x = filesys_exec(tokens, argv, env);
 			if (isatty(STDIN_FILENO))
+			{
 				__free(buffer, tokens);
 				continue;
+			}
 			if (x == -1 && !isatty(STDIN_FILENO))
+			{
 				__free(buffer, tokens);
 				exit(-1);
+			}
 			if (x != -1 && !isatty(STDIN_FILENO))
+			{
 				__free(buffer, tokens);
 				exit(0);
+			}
+		}
 	}
 return (0);
 }
